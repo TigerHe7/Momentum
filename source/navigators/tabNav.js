d836e1d009a80b595ab53e7e334f543b9a0ae481
tigerhe7@gmail.com <the@bhvr.com>
2018-03-27 21:45:42 -0400

    Added basic beginning of layout for profile + goal setting screen

--- source/navigators/tabNav.js
+++ source/navigators/tabNav.js
@@ -8,14 +8,14 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import PlanningScreenNav from './planScreenNav';
 import TodayScreenNav from './todayScreenNav';
 
-import GoalScreen from './../screens/goalScreen';
+import ProfileScreen from './../screens/profileScreen';
 
 import Colors from './../styles/colors';
 
 const routeConfiguration = {
   PlanningScreen: { screen: PlanningScreenNav },
   TodayScreenNav: { screen: TodayScreenNav },
-  GoalScreen: { screen: GoalScreen },
+  ProfileScreen: { screen: ProfileScreen },
 };
 
 const tabBarConfiguration = {
@@ -23,7 +23,7 @@ const tabBarConfiguration = {
     tabBarIcon: ({ tintColor }) => {
       const { routeName } = navigation.state;
       let iconName;
-      if (routeName === 'GoalScreen') {
+      if (routeName === 'ProfileScreen') {
         iconName = 'circular-graph';
         return <Entypo name={iconName} size={25} color={tintColor} />;
       } else if (routeName === 'TodayScreenNav') {
